8fcb63fa27d1478e0fa24aec2c90b95c1901bd8c
edenbr <edenbr@checkpoint.com>
2024-05-15 18:47:43 +0300

    Fix import failure of threat-profile overrides

--- exporting/special_treatment_objects.py
+++ exporting/special_treatment_objects.py
@@ -1,3 +1,6 @@
+from utils import debug_log
+
+
 def handle_fields(obj):
     if obj["type"] == "threat-profile":
         new_items = {}
@@ -11,7 +14,23 @@ def handle_fields(obj):
                 for index in range(len(obj[key])):
                     for sub_index in range(len(obj[key][index]["values"])):
                         new_items[new_key].append({"category": obj[key][index]["name"],
-                                                  "name": obj[key][index]["values"][sub_index]["name"]})
+                                                   "name": obj[key][index]["values"][sub_index]["name"]})
+
+            if key == "overrides":
+                new_overrides = []
+                for override in obj[key]:
+                    protection = override["protection"]
+                    override_obj = override["override"]
+
+                    new_override = {"protection": protection}
+                    for k, v in override_obj.items():
+                        new_override[k] = v
+
+                    new_overrides.append(new_override)
+
+                obj[key] = new_overrides
+
+                debug_log(f"threat profile overrides: {obj[key]}")
         for key in old_keys:
             obj.pop(key)
         for new_key, new_item in new_items.items():
